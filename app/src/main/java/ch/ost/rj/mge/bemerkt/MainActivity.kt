f63fd906e634751ac156f196552de97dcc9983f9
Floris Staub <floris.staub@hsr.ch>
2020-10-13 18:28:21 +0200

    start of UI Design

--- app/src/main/java/ch/ost/rj/mge/bemerkt/MainActivity.kt
+++ app/src/main/java/ch/ost/rj/mge/bemerkt/MainActivity.kt
@@ -1,11 +1,21 @@
 package ch.ost.rj.mge.bemerkt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        new_note.setOnClickListener(View.OnClickListener { view ->
+            intent = Intent(this, EditActivity::class.java)
+            startActivity(intent)
+
+        })
     }
 }
